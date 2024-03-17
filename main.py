fd3e62560f9c391978a42201b72fd154da591b45
utkarshminds <pranav.nerurkar@utkarshminds.com>
2024-03-17 19:49:53 +0530

    second one

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ def load_best_model():
     bestmodel = keras.load_model('bestmodel.keras')
     return bestmodel
 
+def make_prediction(image):
+    img_resized = image.resize((200,200), Image.LANCZOS)
+    image_array = numpy.expand_dims(img_resized, axis=0)
+    bestmodel = load_best_model()
+    preds = bestmodel.predict(image_array)
+
+    class_final = (preds > 0.5).astype("int32")
+
+    if class_final == 0:
+        return 'bachchan'
+    else:
+        return 'srk'
+
 
 st.title('SRK vs Bachchan classifier')
 
